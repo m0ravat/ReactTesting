a6f24fd150ae9a5ff88e45c571af5b37340efc47
Muhammad Ravat <m0ravat736@gmail.com>
2024-08-24 23:07:03 +0100

    created router file

--- react/src/routes.jsx
+++ react/src/routes.jsx
@@ -1,17 +1,22 @@
 import App from "./App";
 import Profile from "./Profile";
-import ErrorPage from "./ErrorPage";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
 
 const routes = [
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage />,
   },
   {
-    path: "profile/:name",
+    path: "profile",
     element: <Profile />,
+    children: [
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
   },
 ];
 
 export default routes;
+
